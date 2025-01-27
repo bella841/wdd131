35ffd5aa86dda356b3bb9aa7d2e6de4fb260fc82
bella841 <isabella.carmen@gmail.com>
2025-01-27 13:35:45 -0700

    fixing cool pics

---
+++ week4ponder/debugging.js
@@ -0,0 +1,20 @@
+const pi = 3.14;
+const radius = 3;
+let area = 0;
+area = radius * radius * pi;
+console.log("area:", area);
+radius = 4;
+area = radius * radius * pi;
+console.log("area:", area)
+
+
+
+
+
+
+
+
+
+
+
+
