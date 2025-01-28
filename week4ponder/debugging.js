379e4aa57d5f81295c50b9be6d98dd239cb0fcda
bella841 <isabella.carmen@gmail.com>
2025-01-28 13:09:40 -0700

    .

--- week4ponder/debugging.js
+++ week4ponder/debugging.js
@@ -1,11 +1,15 @@
 const pi = 3.14;
-const radius = 3;
+radius = 3;
 let area = 0;
 area = radius * radius * pi;
-console.log("area:", area);
+console.log("area1:", area);
 radius = 4;
 area = radius * radius * pi;
-console.log("area:", area)
+console.log("area2:", area)
+
+function circleArea(radius) {
+    area = radius * radius * pi;
+}
 
 
 
