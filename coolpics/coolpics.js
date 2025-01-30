2095c97046578ea20928ead44a2db5c444feac61
bella841 <isabella.carmen@gmail.com>
2025-01-30 15:45:11 -0700

    DOM

---
+++ coolpics/coolpics.js
@@ -0,0 +1,31 @@
+const menubutton = document.querySelector(".menu-button");
+function toggleMenu() {
+    const menu = document.querySelector(".menu")
+    menu.classList.toggle("hide");
+}
+
+menubutton.addEventListener("click", toggleMenu)
+
+function handleResize() {
+    const menu =  document.querySelector(".menu");
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    }
+    else {
+        menu.classList.add("hide");
+    }
+}
+
+handleResize(); 
+window.addEventListener.add("resize", handleResize)
+
+function viewerTemplate(pic, alt) {
+    return div `class="viewer">
+    <button class="close-viewer">X</button>
+    <img src="${pic}" alt="${alt}" class="large-image">
+</div>`;
+}
+
+function viewHandler(event) {
+    
+}
--- week4ponder/debugging.js
+++ week4ponder/debugging.js
@@ -1,16 +1,15 @@
 const pi = 3.14;
-radius = 3;
+let radius = 3;
 let area = 0;
-area = radius * radius * pi;
-console.log("area1:", area);
-radius = 4;
-area = radius * radius * pi;
-console.log("area2:", area)
+
 
 function circleArea(radius) {
-    area = radius * radius * pi;
+    return radius * radius * pi;
+    
 }
 
+area =  circleArea(radius);
+console.log(area);
 
 
 
