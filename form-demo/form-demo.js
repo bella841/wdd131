1434a4e84d6fde53c63a0cf6861d9d93b5795789
bella841 <isabella.carmen@gmail.com>
2025-02-24 09:25:06 -0700

    the creditcard

--- form-demo/form-demo.js
+++ form-demo/form-demo.js
@@ -21,6 +21,7 @@ function togglePaymentDetails(e) {
   } else if (theForm.paymentMethod.value === "paypal") {
     paypalContainer.classList.remove("hide");
     theForm.paypalUsername.required = true;
+    
   }
 }
 
@@ -28,3 +29,36 @@ function togglePaymentDetails(e) {
 document
   .querySelector("#paymentMethod")
   .addEventListener("change", togglePaymentDetails);
+
+  function validateForm(event) {
+    // get a reference to the form. Because we attached a submit event listener to the form itself, we can access the form either through 'event.target', or 'this'
+    const theForm = event.target;
+    // the default behavior for a form submit is to try and navigate to another page where the form would be processed, if a url is not provided it will reload the current page. This sometimes is not desirable behavior. One case when we might do this is if we think there is bad data in the form.
+    // To keep it from happening we can can call e.preventDefault()
+    // You should always give feedback to the user about what whet wrong so they can fix it. We will store the error messages here
+    const errors = [];
+    // start by assuming the form is valid.
+    let isValid = true;
+    // add our validations here
+    if (theForm.paymentMethod.value === "creditCard") {
+      // normally we would need contact the credit card company to verify the number...we are only going to allow one number as valid to keep things simple.
+      if (theForm.creditCardNumber.value !== "1234123412341234") {
+        isValid = false;
+        errors.push("Invalid Credit Card Number");
+      }
+    }
+   
+    // if we ran into any problems above valid will be false.
+    if (!isValid) {
+      //stop the form from being submitted
+      event.preventDefault();
+      // show the errors
+      showErrors(errors);
+      // return false to let the browser know the form was not submitted.
+      return false;
+    }
+  }
+  // attach a submit event handler to the form
+  document
+    .querySelector("#checkoutForm")
+    .addEventListener("submit", validateForm);
